225738eac762d4c6730e5b4f22770e167f220adb
Adityaphadke270599 <aphadke9927@gmail.com>
2024-01-24 15:40:33 +0530

    schemaTypes Code 2nd Commit

--- sanity_backend/schemas/index.js
+++ sanity_backend/schemas/index.js
@@ -1,1 +1,10 @@
-export const schemaTypes = []
+import about from'./about';
+import brands from './brands';
+import contact from './contact';
+import experiences from'./experiences';
+import skills from './skills';
+import testimonials from './testimonials';
+import work from './work';
+import workExperience from'./workExperience';
+export const schemaTypes = [about, brands, contact, experiences, skills, testimonials, work, workExperience];
+
